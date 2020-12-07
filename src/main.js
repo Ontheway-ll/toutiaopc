9f5922f136701a8f32077680b751396f2e0a6b27
liu lei <1206821643@qq.com>
2020-12-07 18:18:21 +0800

    统一配置axios

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import router from './router'
 import ElementUI from 'element-ui'// 引入模块
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import '@/styles/index.less'// 引入全局样式
+import axios from 'axios'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 配置公共的请求头地址
+Vue.prototype.$axios = axios
 new Vue({
   router,
   render: h => h(App)
